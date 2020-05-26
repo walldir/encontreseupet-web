d74da77fedd50757bd0a3b462e3ad210847a721b
waldirneto <walldir@gmail.com>
2020-05-26 09:22:37 -0300

    Avatar

--- src/containers/Home/components/Section4.jsx
+++ src/containers/Home/components/Section4.jsx
@@ -8,7 +8,7 @@ import { page4, assets } from '../data';
 export default function Page4(props) {
   const children = page4.map((item, i) => (
     <QueueAnim type="bottom" key={i.toString()}>
-      <img key="user" className="user" src='https://avatars1.githubusercontent.com/u/12954134?s=460&u=7082a336d461e5a0feb3eeb4141675d6ae36a462&v=4' alt="" />
+      <img key="user" className="user" src='https://avatars2.githubusercontent.com/u/8962359?s=460&u=b99ab990e0619625acbc578007185c272c029718&v=4' alt="" />
       <div key="comment" className="comment">
         {!props.isMobile && <img src={`${assets}/image/home/comments-leftpad-abd23.svg`} alt="" />}
         <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec libero lobortis, hendrerit tortor ut, elementum elit. Vivamus pellentesque diam ut pharetra facilisis.</span>
